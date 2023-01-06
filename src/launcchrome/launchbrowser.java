7aa21465470f73331f4c112a6588c666cb40cb59
tabbyjim <tabithawanjiku1950@gmail.com>
2023-01-05 23:23:16 -0600

    first commit

---
+++ src/launcchrome/launchbrowser.java
@@ -0,0 +1,10 @@
+package launcchrome;
+
+public class launchbrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/launchbrowser/launchchrome.java
@@ -0,0 +1,11 @@
+package launchbrowser;
+
+public class launchchrome{
+
+	public static void main(String[] args) {
+		
+     scanner scn= new Scanner(system.in);
+     webDriver driver= new chromeDriver();
+	}
+
+}
